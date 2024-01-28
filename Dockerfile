b4a7adae1198cf7f29ab0b40575c3c0669b48ae6
Chris Pattison <gitlab@spawn-design.net>
2024-01-28 18:04:13 +0100

    docker base

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,23 @@ FROM python:3.11
 
 WORKDIR /usr/src/app
 
+# Install von Cron
+RUN apt-get update && apt-get -y install cron
+
 # Copy the requirements.txt and install dependencies
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copy the specific files
+COPY docker_cron /etc/cron.d/docker_cron
 COPY config.py ./
 COPY generate_title.py ./
 COPY run.py ./
+
+RUN touch /var/log/cron.log
+RUN crontab /etc/cron.d/docker_cron
+
+CMD cron -f
 
 # Check if config.py exists
 RUN if [ ! -f config.py ]; then echo "config.py not found" && exit 1; fi
--- install.sh
+++ install.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
+# Define a name for your Docker image
+IMAGE_NAME="paperless-ngx-openai-title"
+
+# Build the Docker image
 echo "Building Docker image for paperless-ngx-openai-title..."
-docker build -t paperless-ngx-openai-title:latest .
+docker build -t $IMAGE_NAME .
 
-# Start the docker-compose services
-echo "Starting services with docker-compose..."
-docker-compose up --detach
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image built successfully."
 
-echo "Re-attaching to console logs"
-docker-compose logs -f
-echo "Service are up and running."
+    # Run docker-compose
+    echo "Starting Docker Compose..."
+    docker-compose up -d
+
+    if [ $? -eq 0 ]; then
+        echo "Docker Compose started successfully."
+        echo "Displaying Docker Compose logs..."
+        
+        # Display Docker Compose logs
+        docker-compose logs -f
+    else
+        echo "Error starting Docker Compose."
+    fi
+else
+    echo "Error building Docker image."
+fi
+
+
+#!/bin/bash
+
+# echo "Building Docker image for paperless-ngx-openai-title..."
+# docker build -t paples-oai-title:latest .
+
+# # Start the docker-compose services
+# echo "Starting services with docker-compose..."
+# docker-compose up --detach
+
+# echo "Re-attaching to console logs"
+# docker-compose logs -f
+# echo "Service are up and running."
