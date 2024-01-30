51d1b59c3f41712f56c71d858e2875848f212158
Chris Pattison <gitlab@spawn-design.net>
2024-01-30 14:26:24 +0100

    prompt & tag check

--- generate_title.py
+++ generate_title.py
@@ -6,26 +6,21 @@ import requests
 import config
 
 def process_document(document_id):
-    print('Processing document with ID ' + document_id)
+    print(f'Document ID {document_id}: Processing document')
 
     paperless_base_url = config.paperless['base_url']
     paperless_auth_header = {'Authorization': 'Token ' + config.paperless['api_key']}
 
     # Collect document information from paperless
-    print('Reading document details for document ' + document_id + " from paperless.")
+    print(f'Document ID {document_id}: Reading document details from paperless.')
     document_url_template = paperless_base_url + '/api/documents/{document_id}/'
     document_url = document_url_template.format(document_id=document_id)
     document_response = requests.get(document_url, headers=paperless_auth_header)
     if document_response.status_code == 404:
-        print("Document does not exist!")
+        print(f'Document ID {document_id}: Document does not exist!')
         exit(0)
 
     document_json = document_response.json()
-    
-    # print(document_json)
-    # print('##################################################################################')
-    # print(document_json['tags'])
-
 
     original_document_content = document_json['content']
     original_document_title = document_json['title']
@@ -34,7 +29,7 @@ def process_document(document_id):
     # Check if title matches filter
     title_pattern = config.paperless['title_pattern']
     if not re.match(title_pattern, original_document_title):
-        print(f"Current title \"{original_document_title}\" does not match pattern \"{title_pattern}\". Skipping.")
+        print(f'Document ID {document_id}: Current title "{original_document_title}" does not match pattern "{title_pattern}". Skipping.')
         exit(0)
 
     # Send content to GPT and ask for title
@@ -51,9 +46,11 @@ def process_document(document_id):
     system_role_message = "You are an expert in analyzing texts. Your task is to create a title for " \
                         "the text provided by the user. Be aware that the text may result from an OCR " \
                         "process and contain imprecise segments. Avoid mentioning dates, any form of " \
-                        "monetary values or or specific names (such as individuals or organizations) in the " \
-                        "title. Ensure the title does never exceed 128 characters. Most importantly, generate " \
-                        "the title in " + openai_language + "."
+                        "monetary values or specific names (such as individuals or organizations) in the " \
+                        "title. Ensure the title does never exceed 128 characters." \
+                        "Do not add or insert any special characters at the beginning and end of the title." \
+                        "A bad example is 'title' or '/title/'." \
+                        "Most importantly, generate the title in " + openai_language + "."
 
     request = {
         "model": openai_model,
@@ -67,10 +64,10 @@ def process_document(document_id):
     openai_response_json = openai_response.json()
 
     open_ai_response_content = openai_response_json['choices'][0]['message']['content']
-    print("OpenAI title suggestion: " + open_ai_response_content)
+    print(f'Document ID {document_id}: OpenAI title suggestion: ' + open_ai_response_content)
 
     # Update Title and add note
-    print(f'Saving original title of document with ID {document_id} as note.')
+    print(f'Document ID {document_id}: Saving original title of document as note.')
     document_original_title_note_request = {
         'note': f'Original title: {original_document_title}'
     }
@@ -78,47 +75,42 @@ def process_document(document_id):
     if not result.ok:
         raise AssertionError("Adding original title as note failed.")
 
-    print(f'Updating title and tags of document with ID {document_id}')
+    # print(f'Document ID {document_id}: Updating title and tags of document')
 
-    patch_data = {
-        'title': open_ai_response_content  # New title
-    }
+    # Check if the tag to be removed is set in the config
+    tag_id_to_remove_config = config.paperless.get('generate_titel_tag')
+    tag_id_to_remove = int(tag_id_to_remove_config) if tag_id_to_remove_config else None
 
-    # Performing the PATCH request
+    # Only update tags if the tag to be removed is set
+    if tag_id_to_remove is not None:
+        # Remove the specific tag from the list
+        updated_tags = [tag for tag in original_document_tags if tag != tag_id_to_remove]
+        
+        # Prepare the data for the update (both title and tags)
+        patch_data = {
+            'title': open_ai_response_content,  # New title
+            'tags': updated_tags  # Updated tag list
+        }
+    else:
+        # Prepare the data for the update (title only)
+        patch_data = {
+            'title': open_ai_response_content  # New title
+        }
+
+    # Perform the PATCH request
     update_response = requests.patch(document_url, json=patch_data, headers=paperless_auth_header)
 
-    # Checking the result of the request
+    # Check the result of the request
     if update_response.status_code == 200:
-        print(f"Updated titel.")
+        if tag_id_to_remove is not None:
+            print(f'Document ID {document_id}: Successfully updated title and tags.')
+        else:
+            print(f'Document ID {document_id}: Successfully updated title.')
     else:
-        print(f"Error updating the document with ID {document_id}: Status code {update_response.status_code}")
+        print(f'Document ID {document_id}: Error updating the document! Status code {update_response.status_code}')
 
 
-    # Preparing the data for the update
-    tag_id_to_remove = int(config.paperless['generate_titel_tag'])
-
-    # Removing the specific tag from the list
-    updated_tags = [tag for tag in original_document_tags if tag != tag_id_to_remove]
-
-    # print(f"original_document_tags: {original_document_tags}    |    updated_tags: {updated_tags}")
-
-    # Merging the update data for title and tags
-    patch_data = {
-        'tags': updated_tags  # Updated tag list
-    }
-
-    # Performing the PATCH request
-    update_response = requests.patch(document_url, json=patch_data, headers=paperless_auth_header)
-
-    # Checking the result of the request
-    if update_response.status_code == 200:
-        print(f"Document with ID {document_id} successfully updated.")
-    else:
-        print(f"Error updating the document with ID {document_id}: Status code {update_response.status_code}")
-
-
-    print('Finished document with ID ' + document_id)
-    print('#############################')
+    print(f'Document ID {document_id}: Finished')
 
 if __name__ == "__main__":
     DOCUMENT_ID = os.getenv('DOCUMENT_ID')
